04bfc09d182e545536ba09971bd856f7cc296318
Prathamkumar18 <pratham854305@gmail.com>
2023-11-16 09:23:24 +0530

    Copymove surf

--- Detector/SurfDetector.py
+++ Detector/SurfDetector.py
@@ -1,11 +1,13 @@
 import cv2
+
 from Detector.AbstractDetector import AbstractDetector
-class SiftDetector(AbstractDetector):
-    # blue
+
+class SurfDetector(AbstractDetector):
+    # Green
     image = None
     key_points = None
     descriptors = None
-    color = (0, 0, 255)
+    color = (0, 255, 0)
     distance = cv2.NORM_L2
 
     def __init__(self, image):
@@ -13,7 +15,8 @@ class SiftDetector(AbstractDetector):
         self.detectFeature()
         super().__init__(self.image)
 
+    # feature detect(descriptors and keypoints)
     def detectFeature(self):
-        sift = cv2.SIFT_create()
+        sift = cv2.SIFT_create()  # burayı surf create yap
         gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.key_points, self.descriptors = sift.detectAndCompute(gray, None)
--- GUI/FacadeGui.py
+++ GUI/FacadeGui.py
@@ -4,8 +4,7 @@ from tkinter import filedialog
 import cv2
 import numpy as np
 from PyQt5 import QtGui, QtWidgets, QtCore
-from Detector.SiftDetector import SiftDetector
-
+from Detector.SurfDetector import SurfDetector
 
 class Facade():
     size = 720
@@ -47,20 +46,33 @@ class Facade():
         icon.addPixmap(QtGui.QPixmap("icons/refresh.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.action_Undo.setIcon(icon)
         self.action_Undo.setObjectName("actionUndo")
-
-        icon2 = QtGui.QIcon()
-        self.action_Sift = QtWidgets.QAction(main_window)
-        icon3 = QtGui.QIcon()
-        icon3.addPixmap(QtGui.QPixmap("icons/sift.jpeg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.action_Sift.setIcon(icon3)
-        self.action_Sift.setObjectName("actionSift")
-
-
-
+        self.action_Save = QtWidgets.QAction(main_window)
+        self.action_Save.setObjectName("actionSave")
+        self.action_Surf = QtWidgets.QAction(main_window)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("icons/surf.jpeg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.action_Surf.setIcon(icon1)
+        self.action_Surf.setObjectName("actionSurf")
+        self.action_ZoomIn = QtWidgets.QAction(main_window)
+        icon4 = QtGui.QIcon()
+        icon4.addPixmap(QtGui.QPixmap("icons/zoom_in.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.action_ZoomIn.setIcon(icon4)
+        self.action_ZoomIn.setObjectName("actionZoomIn")
+        self.action_Exit = QtWidgets.QAction(main_window)
+        self.action_Exit.setObjectName("actionExit")
+        self.action_ZoomOut = QtWidgets.QAction(main_window)
+        icon5 = QtGui.QIcon()
+        icon5.addPixmap(QtGui.QPixmap("icons/zoom_out.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.action_ZoomOut.setIcon(icon5)
+        self.action_ZoomOut.setObjectName("actionZoomOut")
         self.q_menu.addAction(self.action_Open)
+        self.q_menu.addAction(self.action_Save)
+        self.q_menu.addAction(self.action_Exit)
         self.menu_bar.addAction(self.q_menu.menuAction())
         self.tool_bar.addAction(self.action_Undo)
-        self.tool_bar.addAction(self.action_Sift)
+        self.tool_bar.addAction(self.action_Surf)
+        self.tool_bar.addAction(self.action_ZoomIn)
+        self.tool_bar.addAction(self.action_ZoomOut)
         self.translateUi(main_window)
         QtCore.QMetaObject.connectSlotsByName(main_window)
 
@@ -69,22 +81,44 @@ class Facade():
         main_window.setWindowTitle(_translate("MainWindow", "CMDF"))
         self.q_menu.setTitle(_translate("MainWindow", "Menu"))
         self.tool_bar.setWindowTitle(_translate("MainWindow", "Operations"))
-        self.action_Open.setText(_translate("MainWindow", "Choose Image"))
-        self.action_Undo.setText(_translate("MainWindow", "Reset"))
-        self.action_Undo.setToolTip(_translate("MainWindow", "Undo"))
-        self.action_Sift.setText(_translate("MainWindow", "SIFT"))
-        self.action_Sift.setToolTip(_translate("MainWindow", "Sift-Based Detection"))
+        self.action_Open.setText(_translate("MainWindow", "Select Image"))
+        self.action_Undo.setText(_translate("MainWindow", "Reset Image"))
+        self.action_Undo.setToolTip(_translate("MainWindow", "Undo Operations"))
+        self.action_Save.setText(_translate("MainWindow", "Save"))
+        self.action_Surf.setText(_translate("MainWindow", "SURF"))
+        self.action_ZoomIn.setText(_translate("MainWindow", "Zoom In"))
+        self.action_ZoomIn.setToolTip(_translate("MainWindow", "Zoom In on the Image"))
+        self.action_Exit.setText(_translate("MainWindow", "Exit"))
+        self.action_Exit.setToolTip(_translate("MainWindow", "Exit the Program"))
+        self.action_ZoomOut.setText(_translate("MainWindow", "Zoom Out"))
+        self.action_ZoomOut.setToolTip(_translate("MainWindow", "Zoom Out from the Image"))
+
         self.action_Open.triggered.connect(self.openImage)
-        self.action_Sift.triggered.connect(self.siftDetector)
         self.action_Undo.triggered.connect(self.undo)
+        self.action_Exit.triggered.connect(self.exit)
+        self.action_Surf.triggered.connect(self.surfDetector)
+        self.action_ZoomOut.triggered.connect(self.zoomOut)
+        self.action_ZoomIn.triggered.connect(self.zoomIn)
+        self.action_Save.triggered.connect(self.saveImage)
 
     @staticmethod
+    def exit():
+        exit()
+
     def undo(self):
         self.image = self.origImage.copy()
         self.showImage(self.image)
 
     def backup(self):
         self.NPundo = self.NPimg
+
+    def zoomIn(self):
+        self.size = int(self.size * 1.2)
+        self.showImage(self.image)
+
+    def zoomOut(self):
+        self.size = int(self.size / 1.2)
+        self.showImage(self.image)
 
     def openImage(self):
         self.backup()
@@ -94,20 +128,33 @@ class Facade():
                                                    title="Select File",
                                                    filetypes=(("jpeg files", "*.jpeg"), ("all files", "*.*")))
         if root.filename:
-            # img = Image.open(root.filename)
             self.image = cv2.imread(root.filename, cv2.IMREAD_COLOR)
             self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
             self.origImage = self.image.copy()
             self.showImage(self.origImage)
 
+    def saveImage(self):
+        root = tk.Tk()
+        root.withdraw()
+        root.filename = filedialog.asksaveasfilename(initialdir="~",
+                                                     title="Save Image",
+                                                     filetypes=(("jpeg files", "*.jpeg"), ("all files", "*.*")))
+        if root.filename:
+            try:
+                save_img = Image.fromarray(self.NPimg.astype('uint8'))
+                save_img.save(root.filename)
+            except ValueError:
+                save_img = Image.fromarray(self.NPimg.astype('uint8'))
+                save_img.save(root.filename + '.png')
+
     def showImage(self, img_show):
         image_profile = QtGui.QImage(img_show, img_show.shape[1], img_show.shape[0], img_show.shape[1] * 3,
-                                     QtGui.QImage.Format_RGB888)  # QImage object
+                                     QtGui.QImage.Format_RGB888)
         image_profile = image_profile.scaled(self.size, self.size, aspectRatioMode=QtCore.Qt.KeepAspectRatio,
                                              transformMode=QtCore.Qt.SmoothTransformation)
         self.label.setPixmap(QtGui.QPixmap.fromImage(image_profile))
 
-    def siftDetector(self):
-        sift = SiftDetector(self.image)
-        self.image = sift.image
+    def surfDetector(self):
+        surf = SurfDetector(self.image)
+        self.image = surf.image
         self.showImage(self.image)
